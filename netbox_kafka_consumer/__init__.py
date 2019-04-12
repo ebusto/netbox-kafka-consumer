7635e126372f5564957111601501bfa958131ba8
Eric Busto <ebusto@nvidia.com>
2019-04-12 11:51:58 -0700

    More flexible matching.

--- netbox_kafka_consumer/__init__.py
+++ netbox_kafka_consumer/__init__.py
@@ -15,7 +15,7 @@ class Client:
 
 		self.logger = logging.getLogger(__name__)
 
-		self.subscriptions = collections.defaultdict(list)
+		self.subscriptions = []
 
 	def poll(self, interval=1.0):
 		consumer = confluent_kafka.Consumer({
@@ -41,34 +41,43 @@ class Client:
 				continue
 		
 			# Extract the payload, then unmarshal from JSON.
-			data = message.value().decode('utf-8')
-			data = json.loads(data)
+			data = json.loads(message.value().decode('utf-8'))
 
 			# Build the pynetbox record from the model.
-			record = pynetbox.core.response.Record(data['model'], api=self.api, endpoint=None)
+			record = pynetbox.core.response.Record(data['model'], self.api, None)
 
 			# Retrieve the callback functions.
 			for callback in self.callbacks(data):
-				data.update({
-					'record': record,
-					'sender': data['class'],
-				})
+				param = {}
+				extra = {'raw': data, 'record': record, 'sender': data['class']}
 
-				# Build the arguments per the callback's signature.
-				args = dict()
-
+				# Build arguments according to the callback's signature.
 				for name in inspect.signature(callback).parameters:
-					args[name] = data.get(name, None)
+					param[name] = data.get(name, extra.get(name, None))
 
-				callback(**args)
+				callback(**param)
 
 			consumer.commit(message)
 	
 		consumer.close()
 
 	def callbacks(self, data):
-		return self.subscriptions[data['class']]
+		return [cb for (fn,cb) in self.subscriptions if fn(data['class'])]
 
-	def subscribe(self, fn, *classes):
-		for c in classes:
-			self.subscriptions[c].append(fn)
+	def matcher(self, spec):
+		if callable(spec):
+			return lambda v: spec(v)
+
+		if isinstance(spec, bool):
+			return lambda v: spec
+
+		if isinstance(spec, list):
+			return lambda v: v in spec
+
+		if isinstance(spec, str):
+			return lambda v: v == spec
+
+		raise Exception('Unhandled match type: {}'.format(type(match)))
+
+	def subscribe(self, spec, cb):
+		self.subscriptions.append((self.matcher(spec), cb))
